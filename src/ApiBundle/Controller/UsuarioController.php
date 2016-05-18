11eb8bbf4c78d37582e8d567e0af4f1e65e7f870
Agustín Alabarce <aj.alabarce@gmail.com>
2016-05-17 21:09:46 -0300

    obtener usuarios por api

--- src/ApiBundle/Controller/UsuarioController.php
+++ src/ApiBundle/Controller/UsuarioController.php
@@ -6,6 +6,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
 use ApiBundle\Entity\Usuario;
 use ApiBundle\Form\UsuarioType;
 
@@ -37,5 +38,30 @@ class UsuarioController extends FOSRestController {
             return $form;
         }
     }
+
+    /**
+     * @return array
+     *
+     * @ApiDoc(
+     *  description="Obtener listado de usuarios",
+     *  resource=true,
+     *  output={"class"="ApiBundle\Entity\Usuario"}
+     * )
+     * @Get("/", name="api_obtener")
+     */
+    public function obtenerAction()
+    {
+        $response = array();
+        $em = $this->getDoctrine()->getManager();
+        $usuarios = $em->getRepository('ApiBundle:Usuario')->findAll();
+
+        if(!$usuarios)
+        {
+             return $this->view(null, 400);
+        }
+
+        return $this->view($usuarios, 200);
+    }
+
 
 }
